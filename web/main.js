275c6eff8887e86cbebd242460f19e129918a286
Ivan <ivanatora@gmail.com>
2022-04-11 22:34:45 +0300

    Frontend: begin with generating graph step

--- web/main.js
+++ web/main.js
@@ -1,3 +1,6 @@
+let labelsCount = {};
+let requestId = null;
+
 async function read_config() {
   const response = await fetch('/config.json');
   const config = await response.json();
@@ -20,6 +23,8 @@ async function request_listing() {
   const result = await response.json();
   console.log('result', result);
   if (result.data.labelsCount) {
+    labelsCount = result.data.labelsCount;
+    requestId = result.data.requestId;
     // clear old options
     const numOptions = document.getElementById('labels').options.length;
     for (let i = 0; i < numOptions; i++) {
@@ -27,14 +32,38 @@ async function request_listing() {
     }
 
     // add new options
-    const labels = Object.keys(result.data.labelsCount);
-    labels.sort((a, b) => result.data.labelsCount[b] - result.data.labelsCount[a]);
-    // TODO: why every subsequent executions create one less option? 
+    const labels = Object.keys(labelsCount);
+    labels.sort((a, b) => labelsCount[b] - labelsCount[a]);
+    // TODO: investigate random order of options elements generated below
     for (const label of labels) {
       const option = document.createElement('option');
       option.value = label;
-      option.text = `${label} - ${result.data.labelsCount[label]} issues`;
+      option.text = `${label} - ${labelsCount[label]} issues`;
       document.getElementById('labels').appendChild(option);
     }
   }
+}
+
+async function generate_graph() {
+  const config = await read_config();
+  const response = await fetch(config.lambda_generate_graph_endpoint.value, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      jira_base_url: document.getElementById("jira_base_url").value,
+      access_cookies: document.getElementById("access_cookies").value,
+      label: document.getElementById("labels").value,
+      requestId: requestId,
+    })
+  });
+  const result = await response.json();
+  console.log('result', result);
+  document.getElementById('graph_url').value = result.data.graph_url;
+}
+
+function change_label(x) {
+  document.getElementById('num_requests').innerHTML = `This will generate ${labelsCount[x.value]/50} requests`;
+  document.getElementById('generate_graph').disabled = false;
 }
